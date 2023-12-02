0480a71b30cbb01c0b17b932beee08287fd0d147
Emma K <emmakurtz2003@gmail.com>
2023-12-01 21:48:00 -0600

    what

---
+++ TechJobs6Persistent/ViewModels/AddEmployerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechJobs6Persistent.ViewModels
+{
+    public class AddEmployerViewModel
+    {
+        [Required]
+        [StringLength(255,MinimumLength = 1)]
+        public string? Name { get; set; }
+
+        [Required]
+        [StringLength(255, MinimumLength = 1)]
+        public string? Location { get; set; }
+
+        public Boolean isError { get; set; } = false;
+        public String Message { get; set; } = String.Empty;
+    }
+}
---
+++ TechJobs6Persistent/ViewModels/AddJobViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TechJobs6Persistent.Models;
+
+namespace TechJobs6Persistent.ViewModels
+{
+    public class AddJobViewModel
+    {
+        [Required]
+        [StringLength(255, MinimumLength = 1)]
+        public string? Name { get; set; }
+
+      
+        public int EmployerId { get; set; }
+
+        public List<Employer> SelectListItem { get; set; } = new List<Employer> ();
+
+        public Boolean isError { get; set; } = false;
+        public String Message { get; set; } = String.Empty;
+    }
+}
